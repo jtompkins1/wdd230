17e839786b03df9d9d2a8907d5b02612e637dfce
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2024-01-17 21:22:12 -0600

    dark mode

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,3 +5,11 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+const myBtn = document.querySelector("#darkBtn");
+const main = document.querySelector("main");
+
+myBtn.addEventListener('click', () => {
+	main.classList.toggle('dark');
+
+});
