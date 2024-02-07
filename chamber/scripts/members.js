607139d6f4c6f7f109c6d205858b1a497e7a0fc8
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2024-02-06 19:53:04 -0600

    directory styled

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -17,47 +17,36 @@ const  displayMembers = (members) => {
         let companyName = document.createElement("h3");
 
         let address = document.createElement("p");
-
         let phone = document.createElement("p");
-
         let website = document.createElement("a");
-
         let logo = document.createElement("img");
-
         let level = document.createElement("p");
-
         let category = document.createElement("p");
-
         let joinDate = document.createElement("p");
 
-
         companyName.textContent = member.companyName;
-
         address.textContent = member.address;
-
         phone.textContent = member.phone;
-
         website.textContent = member.url;
-
         website.setAttribute("href", member.url)
-
         logo.setAttribute("src", `images/${member.img}`);
         logo.setAttribute("alt", companyName)
-
         level.textContent = `Membership Level: ${member.level}`;
 
-        category.textContent = `Business Category: ${member.category}`;
-
+        category.textContent = `Category: ${member.category}`;
         joinDate.textContent = `Member Since: ${member.joinDate}`;
 
         card.appendChild(companyName);
-        card.appendChild(category);
-        card.appendChild(website);
-        card.appendChild(address);
-        card.appendChild(phone);
-        card.appendChild(level);
-        card.appendChild(joinDate);
         card.appendChild(logo);
+        card.appendChild(category);
+
+        card.appendChild(phone);
+        card.appendChild(address);
+        card.appendChild(level);
+
+
+        card.appendChild(joinDate);
+        card.appendChild(website);
 
         cards.appendChild(card);
 
