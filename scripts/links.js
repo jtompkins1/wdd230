d42caa84c726d5f5cbbcc78e81c3ee47e1b74989
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2024-02-07 19:43:52 -0600

    links pre format

--- scripts/links.js
+++ scripts/links.js
@@ -1,14 +1,44 @@
 const baseURL = "https://jtompkins1.github.io/wdd230/";
 const linksURL = "https://jtompkins1.github.io/wdd230/data/links.json";
 
+const activitiesCard = document.querySelector("#activities");
+
 async function getLinksData () {
     const response = await fetch(linksURL);
     const data = await response.json();
 
     //test
-    console.table(data.links);
+    //console.table(data.weeks);
 
-    //displayLinks(data.links);
+    displayLinks(data.weeks);
+}
+
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+
+        let activities = document.createElement("ul");
+        let weekNumbers = document.createElement("li");
+
+        weekNumbers.textContent = `${week.week}:  `;
+        activities.appendChild(weekNumbers);
+
+        week.links.forEach((link) => {
+
+            let task = document.createElement("a");
+
+            task.textContent = link.title;
+            task.setAttribute("href", baseURL + link.url)
+
+            let taskList = document.createElement("li");
+            taskList.appendChild(task);
+            activities.appendChild(taskList);
+
+        })
+        
+        activitiesCard.appendChild(activities);
+
+    });
 }
 
 getLinksData();
